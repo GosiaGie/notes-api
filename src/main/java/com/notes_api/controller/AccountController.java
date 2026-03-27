1fb1ae1dae114caa847c79ce75e8a121b1c24809
Malgorzata Galat <m.galat@rocketmail.com>
2026-03-27 21:59:25 +0100

    added /login endpoint

--- src/main/java/com/notes_api/controller/AccountController.java
+++ src/main/java/com/notes_api/controller/AccountController.java
@@ -1,5 +1,8 @@
 package com.notes_api.controller;
 
+import com.notes_api.user.login.LoginRequest;
+import com.notes_api.user.login.LoginResponse;
+import com.notes_api.user.login.LoginService;
 import com.notes_api.user.register.RegisterRequest;
 import com.notes_api.user.register.RegisterResponse;
 import com.notes_api.user.register.RegisterService;
@@ -15,10 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final RegisterService registerService;
+    private final LoginService loginService;
 
     @Autowired
-    public AccountController(RegisterService registerService) {
+    public AccountController(RegisterService registerService, LoginService loginService) {
         this.registerService = registerService;
+        this.loginService = loginService;
     }
 
     @PostMapping("auth/register")
@@ -26,6 +31,14 @@ public class AccountController {
     public RegisterResponse register(@Valid @RequestBody RegisterRequest request) {
 
        return registerService.register(request);
+
+    }
+
+    @PostMapping("auth/login")
+    @ResponseStatus(HttpStatus.OK)
+    public LoginResponse login(@Valid @RequestBody LoginRequest request) {
+
+        return loginService.login(request);
 
     }
 
