832346c42e4a2ecc81b4411f24873e3ed024028c
Malgorzata Galat <m.galat@rocketmail.com>
2026-03-27 21:57:05 +0100

    added RateLimitProperties to config classes

--- src/main/java/com/notes_api/NotesApiApplication.java
+++ src/main/java/com/notes_api/NotesApiApplication.java
@@ -1,9 +1,13 @@
 package com.notes_api;
 
+import com.notes_api.security.jtw.JwtProperties;
+import com.notes_api.security.bucket4j.RateLimitProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({JwtProperties.class, RateLimitProperties.class})
 public class NotesApiApplication {
 
 	public static void main(String[] args) {
