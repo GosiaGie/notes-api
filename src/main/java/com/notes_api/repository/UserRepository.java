e339bfddd5f8cc794cc84f447238a2860a15a9d5
Malgorzata Galat <m.galat@rocketmail.com>
2026-03-29 01:33:03 +0100

    added method getOrThrow()

--- src/main/java/com/notes_api/repository/UserRepository.java
+++ src/main/java/com/notes_api/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.notes_api.repository;
 
 import com.notes_api.entity.User;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<User, UUID>, UserRepositoryCustom<User, UUID> {
-
+    default User getOrThrow(UUID id) {
+        return findById(id).orElseThrow(() -> new EntityNotFoundException("User not found: " + id));
+    }
 }
